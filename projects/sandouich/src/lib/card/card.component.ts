5186cf7988488b2fefff50a50a55ff87552fceb1
Jean-Philippe Bourdais <jeanphi.bourdais@gmail.com>
2022-05-12 15:55:51 +0200

    add scss

--- projects/sandouich/src/lib/card/card.component.ts
+++ projects/sandouich/src/lib/card/card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'sand-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
 
