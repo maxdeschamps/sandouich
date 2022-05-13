017d5ee0a6bc52dbb8eebb10f25195d7b569e321
Jean-Philippe Bourdais <jeanphi.bourdais@gmail.com>
2022-05-13 16:02:22 +0200

    add button component

--- projects/sandouich/src/lib/button/button.component.ts
+++ projects/sandouich/src/lib/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'sand-button',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
+  @Input() class?: string = "default";
+  @Input() round: boolean = false;
+  @Input() secondary: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.secondary === true) {
+      this.class += " secondary";
+    }
+
+    if (this.round === true) {
+      this.class += " round";
+    }
   }
 
 }
