14c09a57b80a1fb240ded22ce155acc91607ce4f
Benjamin FERIE <benj-f@sfr.fr>
2022-05-31 11:37:29 +0200

    fix bug ouverture modale (#1)
    Modifications de @WellBaik

--- projects/sandouich/src/lib/service/modal.service.ts
+++ projects/sandouich/src/lib/service/modal.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
-  display: Subject<boolean> = new Subject<boolean>();
+  display = new BehaviorSubject(false);
   status = false;
 
   constructor() { }
